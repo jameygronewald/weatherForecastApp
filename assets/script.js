3b49bb1d09d2ee77c389a1b96e4e9604ed27e321
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-16 21:43:04 -0700

    made URL of API call dynamic by including user's search input as the city name query; added dynamic elements generated with text from the data pulled from the weather API; added another nested API call and variables of latitude and longitude gathered from the first API call to get the UV index value and append it to the DOM with the other current day data

--- assets/script.js
+++ assets/script.js
@@ -11,14 +11,33 @@ $(document).ready(function() {
     // EVENT LISTENERS
     $('#searchButton').on('click', function(event) {
         event.preventDefault();
-        let searchInput = $('#citySearch');
-        let queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=207015d3d9ea763c8fa74acf5fe16ce5';
+        $('#currentDayRow').empty();
+        let searchInput = $('#citySearch').val();
+        console.log(searchInput);
+        let queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + searchInput + '&APPID=207015d3d9ea763c8fa74acf5fe16ce5';
         $.ajax({
             url: queryURL,
             method: 'GET'
         }).then(function(response) {
             console.log(response);
+            let latitude = response.coord.lat;
+            let longitude = response.coord.lon;
+            let tempFarenheit = (response.main.temp - 273.15) * 1.8 + 32;
+            $('#currentDayRow').append($('<h3>').text(response.name));
+            $('#currentDayRow').append($('<p>').text('Temperature: ' + tempFarenheit.toFixed(1) + '°F'));
+            $('#currentDayRow').append($('<p>').text('Humidity: ' + response.main.humidity + '%'));
+            $('#currentDayRow').append($('<p>').text('Wind Speed: ' + response.wind.speed + 'MPH'));
+            let queryURL_UV = 'http://api.openweathermap.org/data/2.5/uvi?APPID=207015d3d9ea763c8fa74acf5fe16ce5&lat=' + latitude + '&lon=' + longitude;
+            $.ajax({
+                url: queryURL_UV,
+                method: 'GET'
+            }).then(function(response) {
+                console.log(response.value);
+                $('#currentDayRow').append($('<p>').text('UV Index: ' + response.value));
+
+            })
         });
+        
     });
 
 
