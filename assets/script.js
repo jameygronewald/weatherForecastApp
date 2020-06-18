59645a58a9fa39243e6a76487c8545086eb3ce5c
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-17 17:56:30 -0700

    added date into current day and added a tomorrow variable to format dates for 5 day forecast; only hard-coded in first day across all cards

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     // INITIALIZE GLOBAL VARIABLES
-
-
+    let today = moment().format('MMMM Do YYYY');
+    console.log(today);
+    let tomorrow = moment().add(1, 'days').format('MMMM Do YYYY');
+    console.log(tomorrow);
     // FUNCTIONS
 
 
@@ -22,7 +24,7 @@ $(document).ready(function() {
             let latitude = response.coord.lat;
             let longitude = response.coord.lon;
             let tempFarenheit = ((response.main.temp - 273.15) * 1.8 + 32).toFixed(1);
-            $('#currentDayRow').append($('<h3>').text(response.name));
+            $('#currentDayRow').append($('<h3>').text(response.name + ' (' + today + ')'));
             $('#currentDayRow').append($('<p>').text('Temperature: ' + tempFarenheit + '°F'));
             $('#currentDayRow').append($('<p>').text('Humidity: ' + response.main.humidity + '%'));
             $('#currentDayRow').append($('<p>').text('Wind Speed: ' + response.wind.speed + 'MPH'));
@@ -32,7 +34,17 @@ $(document).ready(function() {
                 method: 'GET'
             }).then(function(response) {
                 console.log(response);
-                $('#currentDayRow').append($('<p>').text('UV Index: ' + response.value));
+                let UVIndex = response.value;
+                /* if (UVIndex < 3) {
+                    UVIndex.addClass('moderate');
+                }
+                else if (UVIndex < 7) {
+                    UVIndex.addClass('warning');
+                }
+                else {
+                    UVIndex.addClass('danger');
+                } */
+                $('#currentDayRow').append($('<p>').text('UV Index: ' + UVIndex));
 
             });
             let queryURLDaily = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=current,minutely,hourly&appid=207015d3d9ea763c8fa74acf5fe16ce5'
@@ -45,7 +57,8 @@ $(document).ready(function() {
                 let i = 0;
                 while (i < 5) {
                     let dailyTempFarenheit = ((response.daily[i].temp.day - 273.15) * 1.8 + 32).toFixed(1);
-                    $('.cardRow').append($('<div class="card col-md-2"><div class="card-body"><h5 class="card-title" id = "day' + i + '"></h5><p class="card-text cardTemp"id = "day' + i + 'Temp"></p><p class="card-text cardHumidity" id = "day' + i + 'Humidity"></p></div></div>'));
+                    $('.cardRow').append($('<div class="card col-md-2 bg-info text-white"><div class="card-body"><h5 class="card-title" id = "day' + i + '"></h5><p class="card-text cardTemp"id = "day' + i + 'Temp"></p><p class="card-text cardHumidity" id = "day' + i + 'Humidity"></p></div></div>'));
+                    $('#day' + i).text(tomorrow);
                     $('#day' + i + 'Temp').text('Temperature: ' + dailyTempFarenheit + '°F');
                     $('#day' + i + 'Humidity').text('Humidity: ' + response.daily[i].humidity + '%');
                     i++;
